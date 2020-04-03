d6f09cbf06a09480d1587b6fd40d17e38de7deec
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-03 17:27:40 -0600

    Adding a Dynamic Ingredient Component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import classes from './BurgerIngredient.module.css';
+
+import * as burgerIngredientTypes from '../../../types/burger/burger-ingredient-types';
+
+const BurgerIngredient = ({ type }) => {
+
+    let ingredient = null;
+
+    switch (type) {
+
+        case burgerIngredientTypes.BREAD_BOTTOM:
+            ingredient = <div className={ classes.BreadBottom }></div>;
+            break;
+
+        case burgerIngredientTypes.BREAD_TOP:
+            ingredient = (
+                <div className={ classes.BreadTop }>
+                    <div className={ classes.Seeds1 }></div>
+                    <div className={ classes.Seeds2 }></div>
+                </div>
+            );
+            break;
+
+        case burgerIngredientTypes.MEAT:
+            ingredient = <div className={ classes.Meat }></div>;
+            break;
+
+        case burgerIngredientTypes.CHEESE:
+            ingredient = <div className={ classes.Cheese }></div>;
+            break;
+
+        case burgerIngredientTypes.SALAD:
+            ingredient = <div className={ classes.Salad }></div>;
+            break;
+
+        case burgerIngredientTypes.BACON:
+            ingredient = <div className={ classes.Bacon }></div>;
+            break;
+
+        default:
+            ingredient = null;
+            break;
+
+    }
+
+    return ingredient;
+
+};
+
+export default BurgerIngredient;
---
+++ src/types/burger/burger-ingredient-types.js
@@ -0,0 +1,6 @@
+export const BREAD_BOTTOM = 'burgerIngredient/bread-bottom';
+export const BREAD_TOP = 'burgerIngredient/bread-top';
+export const MEAT = 'burgerIngredient/meat';
+export const CHEESE = 'burgerIngredient/cheese';
+export const SALAD = 'burgerIngredient/salad';
+export const BACON = 'burgerIngredient/bacon';
