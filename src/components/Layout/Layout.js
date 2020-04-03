70417e2e8deac33e9b7205c6d6bb43725488756b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-03 17:12:48 -0600

    Starting Implementation of The Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 
+import classes from './Layout.module.css';
+
 const Layout = ({ children }) => (
     <Fragment>
         <div>
             Toolbar, SideDrawer, Backdrop components go here
         </div>
-        <main>
+        <main className={ classes.Content }>
             { children }
         </main>
     </Fragment>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+
+class BurgerBuilder extends Component {
+    
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    Burger goes here
+                </div>
+                <div>
+                    Build controls go here
+                </div>
+            </Fragment>
+        );
+    }
+
+}
+
+export default BurgerBuilder;
