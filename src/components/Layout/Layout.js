30ae1cd98296b9b0f7e3400710285ca1b3ad848c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-06 11:01:55 -0600

    Reusing the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
-const Layout = ({ children }) => (
-    <Fragment>
-        <Toolbar />
-        <SideDrawer />
-        <main className={ classes.Content }>
-            { children }
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    };
+
+    sideDrawerClosedHandler = () => this.setState(() => ({ showSideDrawer: false }));
+
+    render() {
+
+        return (
+            <Fragment>
+
+                <Toolbar />
+
+                <SideDrawer 
+                    closed={ this.sideDrawerClosedHandler }
+                    open={ this.state.showSideDrawer }
+                />
+
+                <main className={ classes.Content }>
+                    { this.props.children }
+                </main>
+
+            </Fragment>
+        );
+
+    }
+
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 import classes from './SideDrawer.module.css';
 
-const SideDrawer = () => {
+const SideDrawer = ({ closed, open }) => {
+
+    let sideDrawerClasses = [classes.SideDrawer, classes.Close];
+
+    if (open) {
+        sideDrawerClasses = [classes.SideDrawer, classes.Open];
+    }
 
     return (
-        <div className={ classes.SideDrawer }>
-            <div className={ classes.Logo }>
-                <Logo />
+        <Fragment>
+
+            <Backdrop 
+                show={ open }
+                clicked={ closed } 
+            />
+
+            <div className={ sideDrawerClasses.join(' ') }>
+                <div className={ classes.Logo }>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+
+        </Fragment>
     );
 
 };
