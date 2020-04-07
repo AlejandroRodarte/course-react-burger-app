d3832cbb2056dfcdb794870bbd1070dfaee51f6f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-07 09:27:33 -0600

    Handling Errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 import * as burgerIngredientTypes from '../../types/burger/burger-ingredient-types';
 import burgerIngredientPrices from '../../utils/burger/burger-ingredient-prices';
 
@@ -165,4 +167,4 @@ class BurgerBuilder extends Component {
 
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
