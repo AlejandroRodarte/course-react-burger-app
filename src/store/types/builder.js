f06f394d55e8a236064fdeb00fd5050965422a45
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-10 17:33:27 -0600

    Create new action type constants

--- src/store/types/builder.js
+++ src/store/types/builder.js
@@ -1,3 +1,4 @@
+export const START_SET_INGREDIENTS = 'builder/startSetIngredients';
 export const SET_INGREDIENTS = 'builder/setIngredients';
 export const ADD_INGREDIENT = 'builder/addIngredient';
 export const REMOVE_INGREDIENT = 'builder/removeIngredient';
--- src/store/types/orders.js
+++ src/store/types/orders.js
@@ -1,1 +1,3 @@
+export const START_SET_ORDERS = 'orders/startSetOrders';
 export const SET_ORDERS = 'orders/setOrders';
+export const START_ADD_ORDER = 'orders/startAddOrder';
