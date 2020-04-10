cba2dfd955a5a8a21dce9bffeab8de049596b1f2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-10 11:40:28 -0600

    Handle business logic of burguer builder on reducer

---
+++ src/store/reducers/builder.js
@@ -0,0 +1,46 @@
+import * as BuilderTypes from '../types/builder';
+
+import burgerIngredientPrices from '../../utils/burger/burger-ingredient-prices';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+export default function(state = initialState, action) {
+
+    switch (action.type) {
+
+        case BuilderTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: action.payload.ingredients
+            };
+
+        case BuilderTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.payload.ingredientName]: state.ingredients[action.payload.ingredientName] + 1
+                },
+                totalPrice: state.totalPrice + burgerIngredientPrices[action.payload.ingredientName]
+            };
+
+        case BuilderTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.payload.ingredientName]: state.ingredients[action.payload.ingredientName] <= 0 ? 0 : state.ingredients[action.payload.ingredientName] - 1
+                },
+                totalPrice: state.ingredients[action.payload.ingredientName] <= 0 ? state.totalPrice : state.totalPrice - burgerIngredientPrices[action.payload.ingredientName],
+            };
+    
+
+        default:
+            return state;
+
+    }
+
+};
