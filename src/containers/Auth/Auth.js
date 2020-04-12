5fe9d965af99514f91a86af5093afcaead65eb83
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-12 15:12:26 -0600

    Deleting form handling boilerplate code from Auth component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Button from '../../components/UI/Button/Button';
 
@@ -37,136 +37,21 @@ const controls = {
     }
 };
 
-class Auth extends Component {
+const Auth = ({ formElementsJsx, isFormValid }) => (
+    <div className={ classes.Auth }>
+        <form>
 
-    state = {
-        controls: {
-            email: this.getInputConfig(
-                'input', 
-                { 
-                    type: 'email', 
-                    placeholder: 'Your Email' 
-                }, 
-                '', 
-                { 
-                    required: true,
-                    isEmail: true
-                }, 
-                false,
-                false
-            ),
-            password: this.getInputConfig(
-                'input', 
-                { 
-                    type: 'password', 
-                    placeholder: 'Your Password' 
-                }, 
-                '', 
-                { 
-                    required: true,
-                    minLength: 7
-                }, 
-                false,
-                false
-            )
-        },
-        formIsValid: false
-    };
+            { formElementsJsx }
 
-    inputChangedHandler = (inputName, e) => {
+            <Button 
+                type="Success"
+                disabled={ !isFormValid }
+            >
+                SUBMIT
+            </Button>
 
-        const value = e.target.value;
-
-        this.setState((prevState) => {
-
-            const updatedForm = {
-                ...prevState.controls,
-                [inputName]: {
-                    ...prevState.controls[inputName],
-                    value,
-                    valid: this.checkValidity(value, prevState.controls[inputName].validation),
-                    touched: true
-                }
-            };
-
-            return {
-                controls: updatedForm,
-                formIsValid: Object.keys(updatedForm).every(inputName => updatedForm[inputName].valid)
-            };
-
-        });
-
-    };
-
-    getInputConfig(
-        inputType = 'input', 
-        elementConfig = {}, 
-        value = '', 
-        validation = {}, 
-        valid = false, 
-        touched = false
-    ) {
-        return { 
-            inputType, 
-            elementConfig, 
-            value, 
-            validation, 
-            valid, 
-            touched 
-        };
-    }
-
-    checkValidity(value, rules) {
-
-        let isValid = true;
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        if (rules.isEmail) {
-            const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        return isValid;
-
-    }
-
-    render() {
-
-        return (
-            <div className={ classes.Auth }>
-                <form>
-
-                    { this.props.formElementsJsx }
-
-                    <Button 
-                        type="Success"
-                        disabled={ !this.props.isFormValid }
-                    >
-                        SUBMIT
-                    </Button>
-
-                </form>
-            </div>
-        );
-
-    }
-
-}
+        </form>
+    </div>
+);
 
 export default withFormHandler(Auth, controls);
