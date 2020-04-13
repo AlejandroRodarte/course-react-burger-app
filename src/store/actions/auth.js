08ae9c7b35ca93f10b76d71a3559cfb98dcbf64c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-12 20:08:19 -0600

    Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,13 +1,32 @@
 import * as types from '../types';
 
+import axios from '../../axios/axios-auth';
+
 export const setAuthLoading = () => ({
     type: types.SET_AUTH_LOADING
 });
 
-export const startSetAuth = (credentials) => (dispatch) => {
-    console.log(credentials);
+export const startSetAuth = (credentials) => async (dispatch) => {
+
     dispatch(setAuthLoading());
+
+    try {
+
+        const auth = await axios.post(`:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`, {
+            ...credentials,
+            returnSecureToken: true
+        });
+
+        console.log(auth);
+
+        dispatch(setAuth(auth));
+
+    } catch (e) {
+        dispatch(authFail(e));
+    }
+
 }
+
 export const setAuth = (auth) => ({
     type: types.SET_AUTH,
     payload: {
