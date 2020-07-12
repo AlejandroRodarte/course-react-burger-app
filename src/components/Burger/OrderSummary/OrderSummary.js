4829871a030b35e2690e41919c0a1bdae7e886b2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-07-11 19:32:25 -0600

    Transform OrderSummary to functional component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,63 +1,59 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 
 import Button from '../../UI/Button/Button';
 
 import burgerIngredientNames from '../../../utils/burger/burger-ingredient-names';
 
-class OrderSummary extends Component {
+const OrderSummary = ({ ingredients, price, purchaseCanceled, purchaseContinued }) => {
 
-    render() {
-
-        const ingredientKeyStyles = {
-            textTransform: 'capitalize'
-        };
+    const ingredientKeyStyles = {
+        textTransform: 'capitalize'
+    };
     
-        const ingredientSummaryJsx =
-            Object
-                .keys(this.props.ingredients)
-                .map(
-                    (ingredientKey) => 
-                        <li key={ ingredientKey }>
-                            <span style={ ingredientKeyStyles }>{ burgerIngredientNames[ingredientKey] }</span>: { this.props.ingredients[ingredientKey] }
-                        </li>
-                );
-    
-        return (
-            <Fragment>
-                <h3>
-                    Your Order
-                </h3>
-                <p>
-                    A delicious burger with the following ingredients
-                </p>
-                <ul>
-                    { ingredientSummaryJsx }
-                </ul>
-                <p>
-                    <strong>Total Price: ${ this.props.price.toFixed(2) }</strong>
-                </p>
-                <p>
-                    Continue to Checkout?
-                </p>
-                <Button
-                    clicked={ this.props.purchaseCanceled }
-                    type="Danger"
-                >
-                    CANCEL
-                </Button>
-                <Button
-                    clicked={ this.props.purchaseContinued }
-                    type="Success"
-                >
-                    CONTINUE
-                </Button>
-            </Fragment>
-        );
+    const ingredientSummaryJsx =
+        Object
+            .keys(ingredients)
+            .map(
+                (ingredientKey) => 
+                    <li key={ ingredientKey }>
+                        <span style={ ingredientKeyStyles }>{ burgerIngredientNames[ingredientKey] }</span>: { ingredients[ingredientKey] }
+                    </li>
+            );
 
-    }
+    return (
+        <Fragment>
+            <h3>
+                Your Order
+            </h3>
+            <p>
+                A delicious burger with the following ingredients
+            </p>
+            <ul>
+                { ingredientSummaryJsx }
+            </ul>
+            <p>
+                <strong>Total Price: ${ price.toFixed(2) }</strong>
+            </p>
+            <p>
+                Continue to Checkout?
+            </p>
+            <Button
+                clicked={ purchaseCanceled }
+                type="Danger"
+            >
+                CANCEL
+            </Button>
+            <Button
+                clicked={ purchaseContinued }
+                type="Success"
+            >
+                CONTINUE
+            </Button>
+        </Fragment>
+    );
 
-}
+};
 
 const mapStateToProps = state => ({
     ingredients: state.builder.ingredients,
